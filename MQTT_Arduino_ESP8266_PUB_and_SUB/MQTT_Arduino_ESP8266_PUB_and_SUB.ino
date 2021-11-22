3f6b66052d0a877c883330f5b15f8e385c02d334
Fiki Nabil Mustaqim <94032888+FikiNabil@users.noreply.github.com>
2021-11-22 11:38:32 +0700

    Add files via upload

--- MQTT_Arduino_ESP8266_PUB_and_SUB/MQTT_Arduino_ESP8266_PUB_and_SUB.ino
+++ MQTT_Arduino_ESP8266_PUB_and_SUB/MQTT_Arduino_ESP8266_PUB_and_SUB.ino
@@ -4,8 +4,10 @@
 #include "SoftwareSerial.h"
 #include <PubSubClient.h>
 
-//Initialize Relay PIN
+//Initialize PIN
 int Relay = 2;
+int LDR   = A0;
+int Flame = A1;
 
 char ssid[]        = "delameta";            // your network SSID (name)
 char pass[]        = "bilano1407";        // your network password
@@ -71,9 +73,41 @@ Serial.print("Pesan diterima [");
     }
     if (receivedChar == '0'){
        //Jalankan Perintah0
-       Serial.println("JALANKAN PERINTAH 0");
+       Serial.println("JALANKAN PERINTAH 2");
        digitalWrite(Relay, LOW);
        delay(1000);
+    }
+  }
+  }
+  //==============================================================
+    //==============================================================
+  if (strcmp(topic, "fikinabil14/LDR") == 0) //Membandingkan 2 buah String
+  { 
+    for (int i=0;i<length;i++) {
+    char receivedChar = (char)payload[i];
+    if (receivedChar == '1'){
+       //Jalankan Perintah1
+       Serial.println("JALANKAN PERINTAH 3");
+    }
+    if (receivedChar == '0'){
+       //Jalankan Perintah0
+       Serial.println("JALANKAN PERINTAH 4");
+    }
+  }
+  }
+  //==============================================================
+    //==============================================================
+  if (strcmp(topic, "fikinabil14/Flame") == 0) //Membandingkan 2 buah String
+  { 
+    for (int i=0;i<length;i++) {
+    char receivedChar = (char)payload[i];
+    if (receivedChar == '1'){
+       //Jalankan Perintah1
+       Serial.println("JALANKAN PERINTAH 5");
+    }
+    if (receivedChar == '0'){
+       //Jalankan Perintah0
+       Serial.println("JALANKAN PERINTAH 6");
     }
   }
   }
@@ -83,15 +117,24 @@ Serial.print("Pesan diterima [");
 
 void pushdata(){
   //Ambil Data / Baca Sensor
-  char msgBuffer[20];
+  //char msgBuffer[20];
   int datax=random(10,100);
+  int bacaLDR=analogRead(LDR);
+      bacaLDR=map(bacaLDR,3,973,0,100);
+  int bacaFlame=analogRead(Flame);
+      bacaFlame=map(bacaFlame,5,1015,100,0);
   
   //POST TO SERIAL
   Serial.print("Sensor 1 :");Serial.print(datax);
+  Serial.print("| Sensor LDR :");Serial.print(bacaLDR);
+  Serial.print("| Sensor Flame :");Serial.print(bacaFlame);
   Serial.println();
   
   //POST TO MQTT
-  client.publish("fikinabil14/datax", dtostrf(datax, 6, 2, msgBuffer)); //Convert to String 
+  //client.publish("fikinabil14/datax", dtostrf(datax, 6, 2, msgBuffer)); //Convert to String 
+  client.publish("fikinabil14/datax", String(datax).c_str());
+  client.subscribe("fikinabil14/bacaLDR", String(bacaLDR).c_str());
+  client.subscribe("fikinabil14/bacaFlame", String(bacaFlame).c_str());
 }
 
 void loop() {
@@ -116,6 +159,8 @@ Serial.println("connected");
 
 //client.publish("outpic","Hello World");
 client.subscribe("fikinabil14/lampu1",0);
+client.publish("fikinabil14/LDR",0);
+client.publish("fikinabil14/Flame",0);
 
 } else {
   Serial.print("failed, rc=");
