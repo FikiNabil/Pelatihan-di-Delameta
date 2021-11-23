a85b33e9e0d657b8da8f6bc7b7924d45690de602
Fiki Nabil Mustaqim <94032888+FikiNabil@users.noreply.github.com>
2021-11-23 11:54:45 +0700

    Add files via upload

--- 1.Sensor_Flame/1.Sensor_Flame.ino
+++ 1.Sensor_Flame/1.Sensor_Flame.ino
@@ -1,39 +1,20 @@
 //INISIALISASI
-int pinFlame=A0;
-int pinBuzzer=4;
+int pinFlame=A1;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(pinBuzzer, OUTPUT);
 }
 
 void loop() {
   //BACA DATA
   int bacaFlame=analogRead(pinFlame);
-  bacaFlame=map(bacaFlame,5,1015,100,0);
+  bacaFlame=map(bacaFlame,1000,0,0,100);
+  
 
   //PRINT TO SERIAL
   Serial.print("NILAI SENSOR :");
   Serial.print(bacaFlame);
-  Serial.println(); //UNTUK ENTER
-
-  //LOGIKA
-  if(bacaFlame == 0) {
-    Serial.println("AMAN!");
-    digitalWrite(pinBuzzer, LOW);
-  }
-  else if(bacaFlame >=20 and bacaFlame <=50){
-    Serial.println("WARNING SILAHKAN PERIKSA!!");
-    digitalWrite(pinBuzzer, HIGH);
-    delay(1000);
-    digitalWrite(pinBuzzer, LOW);
-    delay(1000);
-  }
-  else if(bacaFlame >=50){
-    Serial.println("WASPADA HARAP MENGUNGSI!!!");
-    digitalWrite(pinBuzzer, HIGH);
-    delay(200);
-    digitalWrite(pinBuzzer, LOW);
-    delay(200);
-  }
+  Serial.println();
 }
+
+  
--- 1.Sensor_LDR/1.Sensor_LDR.ino
+++ 1.Sensor_LDR/1.Sensor_LDR.ino
@@ -1,6 +1,5 @@
 //INISIALIASI
 int pinLDR=A0;
-int LEDRed=2;
 
 void setup() {
  Serial.begin(9600);
@@ -9,18 +8,10 @@ void setup() {
 void loop() {
   //BACA DATA
   int bacaLDR=analogRead(pinLDR);
-  pinMode(LEDRed, OUTPUT);
 
   //PRINT TO SERIAL
   Serial.print("NILAI :");
   Serial.print(bacaLDR);
   Serial.println();
-
-  //LOGIKA
-  if(bacaLDR <=500){
-    digitalWrite(LEDRed, HIGH);
-  }
-  else {
-    digitalWrite(LEDRed, LOW);
-  }
+  delay(500);
 }
